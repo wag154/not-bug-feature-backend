1edfeae1e1f954dfbedd4e21c4a45b1da2d0b895
Jack <jackkevinperkins03@gmail.com>
2023-05-04 22:00:10 +0100

    add announcement

---
+++ applications/controllers/Announcement/announcementController.py
@@ -0,0 +1,60 @@
+from applications import  create_app
+from applications.database import db
+from flask import request, jsonify
+from flask_restx import Namespace, Resource
+from applications.model.model import Announcement,Creation_event
+
+api = Namespace('announcement', description='announcement operations')
+db = db.instance
+
+@api.route("/<int:id>")
+@api.produces('application/json')
+class Announcements (Resource):
+    def post (self,id):
+        try:
+           info = request.json
+           username = info.get("username")
+           title = info.get("title")
+           message = info.get("message")
+           pinned = info.get("pinned")
+           if not all ([username,title,message]) and pinned != None or pinned == "":
+               raise ValueError("Missing field")
+           
+           pinned = (lambda a,b,c :a if (c.lower() == "true") else b )(True,False,pinned)
+           new_announcement = Announcement(username = username, title = title,message = message,pinned = pinned)
+           db.session.add(new_announcement)
+           db.session.commit()
+
+           new_create_event = Creation_event(project_id = id, announcement_id = new_announcement.id)
+           db.session.add(new_create_event)
+           db.session.commit()
+           return {"id": new_announcement.id},200
+        except Exception as e :
+            return {"message" : str(e)},400
+        finally:
+            db.session.close()
+
+    def put (self,id):
+        try:
+           info = request.json
+           username = info.get("username")
+           title = info.get("title")
+           message = info.get("message")
+           pinned = info.get("pinned")
+           announcement_id = id
+
+           if not all ([username,title,message]) and pinned != None or pinned == "":
+               raise ValueError("Missing field")
+           
+           pinned = (lambda a,b,c :a if (c.lower() == "true") else b )(True,False,pinned)
+           
+           edit_announcement = Announcement.query.filter_by(id = announcement_id).first()
+           
+           edit_announcement.username,edit_announcement.title,edit_announcement.message,edit_announcement.pinned = username,title,message,pinned
+           db.session.commit()
+           return {"message" : "success"}
+        
+        except Exception as e:
+            return {"message" : str(e)},400
+        finally:
+            db.session.close()
--- applications/controllers/__init__.py
+++ applications/controllers/__init__.py
@@ -6,10 +6,9 @@ from applications.controllers.Kanban.kanbanController import api as kanban_contr
 from applications.controllers.Calendar.CalendarControlelr import api as calendar_controller
 from applications.controllers.Project.projectController import api as project_controller
 from applications.controllers.TeamMemberController.TeamMemberController import api as TeamMemberController
+from applications.controllers.Announcement.announcementController import api as AnnouncementController
 
 api = Api(title='My API')
-
-
 
 api.add_namespace(user_controller, path='/user')
 api.add_namespace(default_controller,path="/yes")
@@ -17,3 +16,4 @@ api.add_namespace(kanban_controller, path="/kanban")
 api.add_namespace(calendar_controller,path = "/calendar")
 api.add_namespace(project_controller, path = "/project")
 api.add_namespace(TeamMemberController, path = "/teammember")
+api.add_namespace(AnnouncementController,path ="/announcement")
